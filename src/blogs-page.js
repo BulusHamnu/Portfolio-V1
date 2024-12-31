4120bea5601a97c316430773ac5dba65e6496efe
Bulus Hamnu <hamnubulus@gmail.com>
2024-12-31 17:33:49 +0100

    Increase line height for paragraph, Date and time sementic element for blogs, Increase font size for headings and basic styles editing

--- src/blogs-page.js
+++ src/blogs-page.js
@@ -7,9 +7,9 @@ const blogListCont = document.querySelector(".blog-list")
 let allBlogs = "";
         blogs.forEach( blog => {
             let blogPost = `
-                <article class="blog"style="padding: .5rem;" >
+                <article class="blog" >
                     <a href="${blog.link}"><h4 class="blog-title">${blog.title}.</h4></a>
-                    <p class="blog-date">${blog.date}</p>
+                    <p class="blog-date"><time datetime=${blog.date}>${blog.date}</time></p>
                     <p class="blog-content-preview">${blog.description}</p>
                     <a href="${blog.link}" class="button">Read More..</a>
                 </article>
