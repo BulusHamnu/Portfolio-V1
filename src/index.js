ae4d8f01b0746d48a17d2c854b9d6d6cb9649f5e
BulusHamnu <hamnubulus@gmail.com>
2025-10-07 17:04:49 +0100

    Refactor: merge the index and project_section code for rendering projects into one file

--- src/index.js
+++ src/index.js
@@ -15,55 +15,9 @@ document.querySelector(".copyright-date").innerText = new Date()
   .getFullYear()
   .toString();
 
-/* Function to dynamically load all the projects */
-function getProjects() {
-  if (projects.length === 0) {
+/* Hide project section if there is no projects */
+if (projects.length === 0) {
     document.querySelector(".projects-section").style.display = "none";
-  } else {
-    let allProjects = "";
-
-    projects.forEach((project) => {
-      if (project.latest) {
-        let projectDetails = `
-                <div class="project-card fade-in" data-animationtype="animate" data-projectid=${project.id}>
-                    <div class="project-cover">
-                        <img class="project-cover-placeholder" src="Asserts/images/placeholder-image.jpeg" alt="${project.title}">
-                        <img class="real-project-cover" src="${project.image}" alt="${project.title}">
-                    </div>
-                    <h3 class="project-title">${project.title}</h3>
-                    <p class="project-description">${project.description}</p>
-                </div>
-            
-            `;
-
-        allProjects += projectDetails;
-      }
-    });
-
-    projectListCont.innerHTML = allProjects;
-
-    document.querySelectorAll(".project-card").forEach((projectCard) => {
-      projectCard.addEventListener("click", (event) => {
-        let id = projectCard.dataset.projectid;
-        console.log(id);
-        window.location.href = "project.html?project=" + id;
-      });
-    });
-
-    // listen for cover images load and hide the place holders
-    document.querySelectorAll(".project-cover").forEach((projectCover) => {
-      const placeholderCoverImage = projectCover.querySelector(
-        ".project-cover-placeholder"
-      );
-      const realCoverImage = projectCover.querySelector(".real-project-cover");
-
-      // event listener
-      realCoverImage.addEventListener("load", () => {
-        realCoverImage.classList.add("loaded");
-        placeholderCoverImage.style.display = "none";
-      });
-    });
-  }
 }
 
 function getBlogs() {
@@ -89,7 +43,6 @@ function getBlogs() {
 }
 
 getBlogs();
-getProjects();
 
 /* Form Handling */
 contactForm.addEventListener("submit", (event) => {
--- src/projects_page.js
+++ src/projects_page.js
@@ -1,12 +1,17 @@
 /* Variable */
 import { projects } from "../data/data.js";
 const projectListCont = document.querySelector(".project-list");
+const filename = window.location.pathname.slice(1).split(".")[0];
 
 /* Display All projects */
 let allProjects = "";
-
-projects.forEach((project) => {
-  let projectDetails = `
+if (projects.length === 0) {
+  projectListCont.classList.remove("grid-container");
+  projectListCont.innerHTML = `<h1>No Project Yet.</h1>`;
+} else {
+  projects.forEach((project) => {
+    if (filename === "index" && project.latest === false) return;
+    let projectDetails = `
         <div class="project-card fade-in" data-animationtype="animate" data-projectid=${project.id}>
             <div class="project-cover">
                 <img class="project-cover-placeholder" src="Asserts/images/placeholder-image.jpeg" alt="${project.title}">
@@ -18,28 +23,29 @@ projects.forEach((project) => {
     
     `;
 
-  allProjects += projectDetails;
-});
-
-projectListCont.innerHTML = allProjects;
-
-document.querySelectorAll(".project-card").forEach((projectCard) => {
-  projectCard.addEventListener("click", (event) => {
-    let id = projectCard.dataset.projectid;
-    window.location.href = "project.html?project=" + id;
+    allProjects += projectDetails;
   });
-});
 
-// listen for cover images load and hide the place holders
-document.querySelectorAll(".project-cover").forEach((projectCover) => {
-  const placeholderCoverImage = projectCover.querySelector(
-    ".project-cover-placeholder"
-  );
-  const realCoverImage = projectCover.querySelector(".real-project-cover");
+  projectListCont.innerHTML = allProjects;
 
-  // event listener
-  realCoverImage.addEventListener("load", () => {
-    realCoverImage.classList.add("loaded");
-    placeholderCoverImage.style.display = "none";
+  document.querySelectorAll(".project-card").forEach((projectCard) => {
+    projectCard.addEventListener("click", (event) => {
+      let id = projectCard.dataset.projectid;
+      window.location.href = "project.html?project=" + id;
+    });
   });
-});
+
+  // listen for cover images load and hide the place holders
+  document.querySelectorAll(".project-cover").forEach((projectCover) => {
+    const placeholderCoverImage = projectCover.querySelector(
+      ".project-cover-placeholder"
+    );
+    const realCoverImage = projectCover.querySelector(".real-project-cover");
+
+    // event listener
+    realCoverImage.addEventListener("load", () => {
+      realCoverImage.classList.add("loaded");
+      placeholderCoverImage.style.display = "none";
+    });
+  });
+}
