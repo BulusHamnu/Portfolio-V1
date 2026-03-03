8717d8228b86038f31c2813332aed06615f996e5
BulusHamnu <hamnubulus@gmail.com>
2026-03-03 13:09:35 +0100

    Fix: updated project detail display in project-detail.js to check if the project does not exists and show project not found, also refactored display code logic.

--- src/project-details.js
+++ src/project-details.js
@@ -6,51 +6,51 @@ const projectContainer = document.querySelector(".project-details");
 let url = new URL(window.location.href);
 let projectId = url.searchParams.get("project");
 
-if (projectId) {
-  projects.forEach((project) => {
-    if (project.id === parseInt(projectId)) {
-      let techTypes = "";
-      project.technologies.forEach((tech) => {
-        let techType = "";
-        techType = `<span>${tech}</span>`;
-        techTypes += techType;
-      });
+const id = parseInt(projectId);
+const selectedProject = projects.find((project) => project.id === id);
 
-      let projectDetail = `
-                <h3><a href="projects.html">Back to projects</a></h3>
-                    <h1 class="fade-in" data-animationtype="animate">${project.title}</h1>
-                    <img class="image-cover-placeholder fade-in" data-animationtype="animate" src="Asserts/images/placeholder-image.jpeg" alt="${project.title}">
-
-                    <img class="real-project-image loaded fade-in" data-animationtype="animate" src=${project.image} alt="${project.title}">
-
-                    <div class="tags fade-in" data-animationtype="animate">${techTypes}</div>
-                    <div class="project-buttons fade-in" data-animationtype="animate">
-                        <a href="${project.link.demoUrl}" class="button projectDemo" target="_blank">Demo</a>
-                        <a href="${project.link.git_hubUrl}" class="button-transparent" target="_blank">Code</a>
-                    </div>
-                    
-                    <h2 class"fade-in" data-animationtype="animate">Description</h2>
-                    <p class="fade-in" data-animationtype="animate">${project.description}.</p>
-                
-            `;
-
-      projectContainer.innerHTML = projectDetail;
-      if (!project.link.demoUrl) {
-        document.querySelector(".projectDemo").style.display = "none";
-      }
-
-      // listen for cover images load and hide the place holders
-      const placeholderCoverImage = document.querySelector(
-        ".image-cover-placeholder",
-      );
-      const realCoverImage = document.querySelector(".real-project-image");
-
-      // event listener
-      realCoverImage.addEventListener("load", () => {
-        placeholderCoverImage.style.display = "none";
-      });
-    }
-  });
+if (!projectId || !selectedProject) {
+  projectContainer.innerHTML =
+    "<H1 style='text-align: center;'>Project not found.</H1>";
+  projectContainer.style = "margin-top: 7rem";
 } else {
-  projectContainer.innerHTML = "<H1>Folder is Empty</H1>";
+  let techTypes = "";
+  selectedProject.technologies.forEach((tech) => {
+    let techType = "";
+    techType = `<span>${tech}</span>`;
+    techTypes += techType;
+  });
+
+  let projectDetail = `
+              <h3><a href="projects.html">Back to projects</a></h3>
+              <h1 class="fade-in" data-animationtype="animate">${selectedProject.title}</h1>
+
+              <img class="project-cover-image-placeholder fade-in" data-animationtype="animate" src="asserts/images/placeholder-image.jpeg" alt="${selectedProject.title}">
+
+              <img class="real-project-cover-image loaded fade-in" data-animationtype="animate" src=${selectedProject.image} alt="${selectedProject.title}">
+
+              <div class="tags fade-in" data-animationtype="animate">${techTypes}</div>
+              <div class="project-buttons fade-in" data-animationtype="animate">
+                  <a href="${selectedProject.link.demoUrl}" class="button projectDemo" target="_blank">Demo</a>
+                  <a href="${selectedProject.link.git_hubUrl}" class="button-transparent" target="_blank">Code</a>
+              </div>
+              
+              <h2 class"fade-in" data-animationtype="animate">Description</h2>
+              <p class="fade-in" data-animationtype="animate">${selectedProject.description}.</p>
+              
+          `;
+
+  projectContainer.innerHTML = projectDetail;
+  if (!selectedProject.link.demoUrl) {
+    document.querySelector(".projectDemo").style.display = "none";
+  }
+
+  // For monitoring project cover image
+  const placeholderCoverImage = document.querySelector(
+    ".project-cover-image-placeholder",
+  );
+  const realCoverImage = document.querySelector(".real-project-cover-image");
+  realCoverImage.addEventListener("load", () => {
+    placeholderCoverImage.style.display = "none";
+  });
 }
